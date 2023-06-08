4ddddc24e12716afabeeb1860c4f13f5dc5b9043
lukebabbitt <92278416+lukebabbitt@users.noreply.github.com>
2023-06-08 15:19:14 -0700

    incomplete data clean

--- data_clean.py
+++ data_clean.py
@@ -1,19 +1,33 @@
 import pandas as pd
 
-df = pd.read_csv('pitchbook_data.csv')
+df = pd.read_csv('PitchBook - Morningstar - Unicorn prediction data.csv')
 
-df['Emerging Spaces'] = df['Emerging Spaces'].fillna(0)
-df = df.dropna()
+print(df[df['Round Entity ID'] == 587392]['Deal Type 2'])
 
-for dates_col in ['First Financing Date', 'Last Known Valuation Date']:
-    df[dates_col] = pd.DatetimeIndex(df[dates_col])
-    df[dates_col]= df[dates_col].dt.year + ((df[dates_col].dt.dayofyear - 1) / 365)
+# print(df[df['Round Entity ID'] == 587392]['Deal Type 2'])
 
-df = df.replace(',','', regex=True)
+for i in df.shape[0]:
+    print
 
-for float_col in ['Last Known Valuation', 'First Financing Valuation', 'First Financing Size']:
-    df[float_col] = df[float_col].astype(float)
+print(df['Deal Type 2'])
 
-df['Unicorn Status'] = 1 * (df['Last Known Valuation'] >= 1000)
+if df['Deal Type 2'].startswith('Series A'):
+    df['Deal Type 2'] = 'Series A'
+
+print(df[df['Round Entity ID'] == 587392]['Deal Type 2'])
+
+# df['Emerging Spaces'] = df['Emerging Spaces'].fillna(0)
+# df = df.dropna()
+
+# for dates_col in ['First Financing Date', 'Last Known Valuation Date']:
+#     df[dates_col] = pd.DatetimeIndex(df[dates_col])
+#     df[dates_col]= df[dates_col].dt.year + ((df[dates_col].dt.dayofyear - 1) / 365)
+
+# df = df.replace(',','', regex=True)
+
+# for float_col in ['Last Known Valuation', 'First Financing Valuation', 'First Financing Size']:
+#     df[float_col] = df[float_col].astype(float)
+
+# df['Unicorn Status'] = 1 * (df['Last Known Valuation'] >= 1000)
 
 df.to_csv('cleaned_data.csv', index=False)
