9ed5c23ab6db8d77f9fd93b4531786f04086c915
lukebabbitt <92278416+lukebabbitt@users.noreply.github.com>
2023-06-10 03:25:12 -0700

    Here she is....

--- data_vis.py
+++ data_vis.py
@@ -61,14 +61,21 @@ for name in data_frames:
     plt.savefig(save_path)
     plt.close()
 
-# scatter_plot(df['First Financing Date'], df['First Financing Valuation'], './plots/first_financing.png')
-# scatter_plot(df['Last Known Valuation Date'], df['Last Known Valuation'], './plots/last_valuation.png')
 
-# # maybe we don't need these and just keep density histograms 
-# histogram(df['First Financing Date'], bins=15, save_path='./plots/first_financing_date_hist.png')
-# histogram(df['First Financing Valuation'], bins=15, save_path='./plots/first_financing_val_hist.png')
-# histogram(df['First Financing Size'], bins=15, save_path='./plots/first_financing_size_hist.png')
 
-# density_histogram(df['First Financing Date'], bins=15, save_path='./plots/first_financing_date_dhist.png')
-# density_histogram(df['First Financing Valuation'], bins=15, save_path='./plots/first_financing_val_dhist.png')
-# density_histogram(df['First Financing Size'], bins=15, save_path='./plots/first_financing_size_dhist.png')
+
+
+
+
+
+# # scatter_plot(df['First Financing Date'], df['First Financing Valuation'], './plots/first_financing.png')
+# # scatter_plot(df['Last Known Valuation Date'], df['Last Known Valuation'], './plots/last_valuation.png')
+
+# # # maybe we don't need these and just keep density histograms 
+# # histogram(df['First Financing Date'], bins=15, save_path='./plots/first_financing_date_hist.png')
+# # histogram(df['First Financing Valuation'], bins=15, save_path='./plots/first_financing_val_hist.png')
+# # histogram(df['First Financing Size'], bins=15, save_path='./plots/first_financing_size_hist.png')
+
+# # density_histogram(df['First Financing Date'], bins=15, save_path='./plots/first_financing_date_dhist.png')
+# # density_histogram(df['First Financing Valuation'], bins=15, save_path='./plots/first_financing_val_dhist.png')
+# # density_histogram(df['First Financing Size'], bins=15, save_path='./plots/first_financing_size_dhist.png')
